60e720714a51a442638af770b8026298ff234ab7
metaIhead <vipkingston1000@mail.ru>
2018-02-21 10:54:42 +0800

    обновление окна и три датчика

--- Logic/1.py
+++ Logic/1.py
@@ -19,7 +19,7 @@ def coordinatStream ():
             i += step
 
     while True:
-        time.sleep(1)
+
         host1 = '192.168.1.131' #хост датчика
         host2 = '192.168.1.132' #хост датчика
         host3 = '192.168.1.133' #хост датчика
@@ -27,21 +27,33 @@ def coordinatStream ():
 
         adres =('192.168.1.150',55555)
         com=34#комманда отправить профиль из защёлки
+
+
         i=struct.pack('B',com)#паковка  команды
 
         sock=socket.socket(socket.AF_INET,
                        socket.SOCK_DGRAM)
         sock.bind(adres)
-        sock.sendto(i, (host1, p)) #отсылка команды
-        sock.sendto(i, (host2, p)) #отсылка команды
-        sock.sendto(i, (host3, p)) #отсылка команды
-        print('отправил')
-        i1, addr = sock.recvfrom(4104)
-        l=sock.getsockname()#получение ip и порта откуда был сделан запрос в вормате tupel
-        print("port, ip destination",l)
-        #print(i1)
-        print("__________")
+        def send (i,host,p):
+            sock.sendto(i, (host1, p)) #отсылка команды
+            i1, addr = sock.recvfrom(4104)
+            print("_____________")
+            return(i1,addr)
 
+
+        send(i,host1,p)
+        time.sleep(0.4)
+
+        send(i,host2,p)
+        time.sleep(0.4)
+
+        send(i,host3,p)
+        time.sleep(0.4)
+
+        #l=sock.getsockname()#получение ip и порта откуда был сделан запрос в вормате tupel
+        print("port, ip destination",l)
+        print("__________")
+        '''
         n=1
         mas=[]
         for i in get_slice():
@@ -52,5 +64,5 @@ def coordinatStream ():
             mas.append(x)#добавляет в конец стиска значение растояние
             mas.append(y)#добавляет в конец списка боковое смещение
             n=n+1
-
+        '''
         return(mas)
